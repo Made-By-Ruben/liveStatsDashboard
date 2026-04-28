c3863fa31bf576cfb5dc3a436d3eb37b823cde9d
FalloutCS <ruben.leopold@gmail.com>
2026-04-28 23:30:37 +0200

    improved +page.server.ts for TotalDamage visual to be more readable and easier to change

--- src/routes/liveVisuals/TotalDamageDoneVisualV2/+page.server.ts
+++ src/routes/liveVisuals/TotalDamageDoneVisualV2/+page.server.ts
@@ -1,30 +1,33 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
-	const reqBody = JSON.stringify({
-		isCustom: false,
-		visualName: 'TOTAL_DAMAGE_DONE_H2H'
-	});
+const config = JSON.stringify({
+	isCustom: false,
+	visualName: 'TOTAL_DAMAGE_DONE_H2H'
+});
+const matchID = 140;
 
-	const apiResponse = await fetch('http://localhost:3000/getVisual/104', {
+export const load = (async () => {
+	const data = await getDefaultVisual(config, matchID);
+	return {
+		team1: data.data[100],
+		team2: data.data[200],
+		maxDamage: data.data.maxDamage
+	};
+}) satisfies PageServerLoad;
+
+async function getDefaultVisual(config: string, matchID: number) {
+	const apiResponse = await fetch(`http://localhost:3000/getVisual/${matchID}`, {
 		method: 'POST',
 		headers: {
 			'X-Powered-By': 'Express',
 			'Content-Type': 'application/json; charset=utf-8'
 		},
-		body: reqBody
+		body: config
 	});
 
-	if (apiResponse.ok) {
-		const data = await apiResponse.json();
-		console.log(data)
-		return {
-			team1: data.data[100],
-			team2: data.data[200],
-			maxDamage: data.data.maxDamage
-		};
-	} else {
+	if (!apiResponse.ok) {
 		error(apiResponse.status);
 	}
-}) satisfies PageServerLoad;
+	return await apiResponse.json();
+}
