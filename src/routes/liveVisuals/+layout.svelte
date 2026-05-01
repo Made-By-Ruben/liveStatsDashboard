3701d9778e84d1d2d76f27b9319f7b38259be8b5
FalloutCS <ruben.leopold@gmail.com>
2026-05-01 21:51:10 +0200

    started working on stinger in the application

--- src/routes/liveVisuals/+layout.svelte
+++ src/routes/liveVisuals/+layout.svelte
@@ -1,15 +1,34 @@
 <script lang="ts">
+	import Stinger from '$lib/components/Stinger.svelte';
 	import type { LayoutProps } from '../$types';
 
+
 	let { data, children }: LayoutProps = $props();
+
+	let animationPlaying = $state(true);
+	$effect(()=> {
+		setTimeout((() => animationPlaying = false), 2000)
+	})
+
 </script>
 
-<main class="flex h-270 w-480 flex-col justify-end bg-[url(/src/lib/assets/Background.avif)]">
-	<div class="flex h-64.5 w-full">
-		<div class="w-75.5"></div>
-		<div class="w-328 flex-col border border-border-blue">
+<!-- Current config for stream:
+	Aspect Ratio:
+		Total width: 1920px
+		Total height: 1080px
+	Lower HUD:
+		Height: 258px
+		left el width: 302px
+		middle el width: 1338px
+		right el width: 280px  
+-->
+
+<main class="relative h-270 w-480 bg-[url(/src/lib/assets/Background.avif)]">
+	<div class="absolute bottom-0 left-75.5 flex h-64.5 w-334.5 flex-col border border-border-blue">
+		{#if animationPlaying}
+			<Stinger />
+		{:else}
 			{@render children()}
-		</div>
-		<div class="w-76.5"></div>
+		{/if}
 	</div>
 </main>
