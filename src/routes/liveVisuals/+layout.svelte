2f04265993e4645cf3fa8811c2698b04dc016447
FalloutCS <ruben.leopold@gmail.com>
2026-05-01 02:20:50 +0200

    updated sizing

--- src/routes/liveVisuals/+layout.svelte
+++ src/routes/liveVisuals/+layout.svelte
@@ -5,12 +5,11 @@
 </script>
 
 <main class="flex h-270 w-480 flex-col justify-end bg-[url(/src/lib/assets/Background.avif)]">
-	<div class="flex h-62 w-full">
-		<div class=" w-75.5 border border-border-blue"></div>
-		<div class="62 w-visual-width flex-col border border-border-blue">
+	<div class="flex h-64.5 w-full">
+		<div class="w-75.5 border border-border-blue"></div>
+		<div class="w-328 flex-col border border-border-blue">
 			{@render children()}
 		</div>
-		<div class=" w-76.5 border border-border-blue"></div>
+		<div class="w-76.5 border border-border-blue"></div>
 	</div>
 </main>
-
