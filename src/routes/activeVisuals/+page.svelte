62e0fb9e561a51397c6c4434fa609c44fc54ab09
FalloutCS <ruben.leopold@gmail.com>
2026-05-20 18:27:26 +0200

    added an activeVisuals route with a state for the active Visual

---
+++ src/routes/activeVisuals/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { PageProps } from './$types';
+
+    let { data }: PageProps = $props();
+
+	let activeVisual = $state<null | "totalDmgVisual" | "customVisual">(null)
+</script>
+
+<main class="relative h-270 w-480">
+	<div
+		class="mainContainer absolute bottom-0 left-75.5 flex h-62.5 w-334.5 flex-col border border-border-blue bg-transparent"
+	>
+		{#if activeVisual === 'totalDmgVisual'}
+			
+		{/if}
+
+	</div>
+</main>
