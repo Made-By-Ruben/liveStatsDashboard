f44ff5f6939140d4b79af831644fd758296c7e6f
FalloutCS <ruben.leopold@gmail.com>
2026-05-22 18:23:52 +0200

    changed tab icon to ROL icon and updated gitignore

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import icon from '$lib/assets/icon.png';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href={icon} /></svelte:head>
 {@render children()}
