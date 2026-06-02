94394a74553422aec58ca0cb35242f7c833c5597
FalloutCS <ruben.leopold@gmail.com>
2026-06-03 00:13:33 +0200

    fixed issue where accidently cached the portrait image instead of the square image

--- src/routes/activeVisuals/+page.svelte
+++ src/routes/activeVisuals/+page.svelte
@@ -49,7 +49,7 @@
 	function cacheImages(data: ActiveChamps) {
 		data.forEach(champ => {
 			const portrait = new Image()
-			portrait.src = `${PUBLIC_CDN_URL}${champ.championName}/portrait`
+			portrait.src = `${PUBLIC_CDN_URL}${champ.championName}/square`
 
 			const splash = new Image();
 			splash.src = `${PUBLIC_CDN_URL}${champ.championName}/splash-art`
